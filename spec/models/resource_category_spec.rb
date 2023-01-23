f306d06605a37544d5717752b66a475bfc64efe8
grassele <91402260+grassele@users.noreply.github.com>
2023-01-22 23:31:35 -0800

    resource category name and active tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
-  it "exists" do
-    ResourceCategory.new
-  end
+  # it "exists" do
+  #   ResourceCategory.new
+  # end
+
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:active) }
+
 
 end
