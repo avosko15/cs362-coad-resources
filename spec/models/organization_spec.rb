97e20801f9fca958e6afda6cdf83c31e8749d7e5
unknown <pughbl@oregonstate.edu>
2023-01-22 19:44:54 -0800

    Name respond-to matcher test created and passes

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
-  it "exists" do
-    Organization.new
-  end
+  # it "exists" do
+  #   Organization.new
+  # end
+
+  it { is_expected.to respond_to(:name) }
 
 end
