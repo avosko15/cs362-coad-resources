48f4b1831446499362e0749067f4274eedb02ecc
grassele <91402260+grassele@users.noreply.github.com>
2023-01-22 23:29:46 -0800

    region name test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -12,12 +12,11 @@ RSpec.describe Organization, type: :model do
   it { is_expected.to respond_to(:email) }
   it { is_expected.to respond_to(:description) }
   it { is_expected.to respond_to(:rejection_reason) }
-  it { is_expected.to respond_to(:name) }
   it { is_expected.to respond_to(:liability_insurance) }
   it { is_expected.to respond_to(:primary_name) }
   it { is_expected.to respond_to(:secondary_name) }
   it { is_expected.to respond_to(:secondary_phone) }
   it { is_expected.to respond_to(:title) }
   it { is_expected.to respond_to(:transportation) }
-  
+
 end
