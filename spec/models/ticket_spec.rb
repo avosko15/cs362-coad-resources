e21b347625d6c82bb23a78a82678ceebae04c759
grassele <91402260+grassele@users.noreply.github.com>
2023-01-22 23:33:08 -0800

    ticket name and description tests

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,8 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-  it "exists" do
-    Ticket.new
-  end
+  # it "exists" do
+  #   Ticket.new
+  # end
+
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:description) }
+  
+  # t.string "name"
+  # t.string "description"
+  # t.string "phone"
+  # t.integer "organization_id"
+  # t.datetime "created_at", null: false
+  # t.datetime "updated_at", null: false
+  # t.boolean "closed", default: false
+  # t.datetime "closed_at"
+  # t.integer "resource_category_id"
+  # t.integer "region_id"
 
 end
