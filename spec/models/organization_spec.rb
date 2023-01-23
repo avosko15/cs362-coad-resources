371de6d4880cf6845ec2f969f6a92bacc79c9b96
grassele <91402260+grassele@users.noreply.github.com>
2023-01-22 23:16:46 -0800

    organization status test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -7,5 +7,18 @@ RSpec.describe Organization, type: :model do
   # end
 
   it { is_expected.to respond_to(:name) }
-
+  it { is_expected.to respond_to(:status) }
+  # t.integer "status"
+  # t.string "phone"
+  # t.string "email"
+  # t.string "description"
+  # t.string "rejection_reason"
+  # t.datetime "created_at", null: false
+  # t.datetime "updated_at", null: false
+  # t.boolean "liability_insurance", default: false
+  # t.string "primary_name"
+  # t.string "secondary_name"
+  # t.string "secondary_phone"
+  # t.string "title"
+  # t.integer "transportation"
 end
