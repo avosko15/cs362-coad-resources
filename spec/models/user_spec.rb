e7852b84dc69569f7be093e38d77003dd9f10705
grassele <91402260+grassele@users.noreply.github.com>
2023-01-22 23:36:07 -0800

    user email and role tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it "exists" do
-    User.new
-  end
+  # it "exists" do
+  #   User.new
+  # end
+
+  it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:role) }
 
 end
