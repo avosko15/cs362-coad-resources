6f3fff8d28d5f690a130a435ecf29604460c686a
grassele <91402260+grassele@users.noreply.github.com>
2023-01-26 11:37:01 -0800

    region association validation tests

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -22,5 +22,9 @@ RSpec.describe Region, type: :model do
 
   # associations
   it { is_expected.to have_many(:tickets) }
+  
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
 
 end
