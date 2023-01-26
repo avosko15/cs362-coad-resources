0ea64c1dd1686a58c0050bb77c1ceedc89ad4147
grassele <91402260+grassele@users.noreply.github.com>
2023-01-26 11:50:01 -0800

    some user association validation tests and new Gemfile shoulda-matcher version

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,13 @@ RSpec.describe User, type: :model do
   it { is_expected.to respond_to(:role) }
 
   # associations
-  # it { is_expected.to belong_to(:organization).optional }
+  it { is_expected.to belong_to(:organization).optional }
 
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create) }
+  # validates :email, format: { with: VALID_EMAIL_REGEX }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_presence_of(:password).on(:create) }
+  it { is_expected.to validate_length_of(:password).is_at_least(7).is_at_most(255).on(:create) }
 
 end
