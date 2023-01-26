9026d3387b55da974512afeadd806337a65b1b10
grassele <91402260+grassele@users.noreply.github.com>
2023-01-26 11:38:56 -0800

    resource category association validation tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -14,5 +14,8 @@ RSpec.describe ResourceCategory, type: :model do
   it { is_expected.to have_and_belong_to_many(:organizations) }
   it { is_expected.to have_many(:tickets) }
 
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
 
 end
