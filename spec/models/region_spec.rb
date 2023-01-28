90d5d4ba8a88412e04423b247ce7970f584232a1
grassele <91402260+grassele@users.noreply.github.com>
2023-01-28 14:13:42 -0800

    region static function test working

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+  # instantiation - 2.2
   # it "exists" do
   #   Region.new
   # end
@@ -17,25 +18,27 @@ RSpec.describe Region, type: :model do
   #   result = region.to_s
   # end
 
+  # attributes used in database - 2.4
+  it { is_expected.to respond_to(:name) }
+
+  # associations - 2.6
+  it { is_expected.to have_many(:tickets) }
+  
+  # validations - 3.1
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+
+  # member functions - 3.2
   it "returns its own name" do
     r = Region.new(name: "Sisters")
     expect(r.to_s).to eq("Sisters")
   end
 
-  # class functions
-  # def self.unspecified
-  #   Region.find_or_create_by(name: 'Unspecified')
-  # end
-
-  # attributes used in database
-  it { is_expected.to respond_to(:name) }
-
-  # associations
-  it { is_expected.to have_many(:tickets) }
-  
-  # validations
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
-  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  # class functions - 3.3
+  it "can find or create Region record with name 'Unspecified'" do
+    r = Region.unspecified
+    expect(r.name).to eq("Unspecified")
+  end
 
 end
