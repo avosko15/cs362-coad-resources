f26f9007816472f0d8fe2a18102078b7eccb2435
Anna <73409556+avosko15@users.noreply.github.com>
2023-02-01 10:22:41 -0800

    erge error trying to resolve

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -79,40 +79,5 @@ RSpec.describe Ticket, type: :model do
     expect(Ticket.open).to eq([open_ticket])
   end
   
-  it "should return only closed resources" do
-    closed_resource = Resource.create(closed: true)
   
-    expect(Resource.closed).to eq([closed_resource])
-  end
   
-  it "should return only resources where organization_id is not nil" do
-    all_organization_resource = Resource.create(closed: false, organization_id: 1)
-  
-    expect(Resource.all_organization).to eq([all_organization_resource])
-  end
-  
-  it "should return only resources where organization_id is the specified value and closed is false" do
-    organization_resource = Resource.create(organization_id: 1, closed: false)
-  
-    expect(Resource.organization(1)).to eq([organization_resource])
-  end
-  
-  it "should return only resources where organization_id is the specified value and closed is true" do
-    closed_organization_resource = Resource.create(organization_id: 1, closed: true)
-  
-    expect(Resource.closed_organization(1)).to eq([closed_organization_resource])
-  end
-  
-  it "should return only resources where region_id is the specified value" do
-    region_resource = Resource.create(region_id: 1)
-  
-    expect(Resource.region(1)).to eq([region_resource])
-  end
-  
-  it "should return only resources where resource_category_id is the specified value" do
-    resource_category_resource = Resource.create(resource_category_id: 1)
-  
-    expect(Resource.resource_category(1)).to eq([resource_category_resource])
-  end
-  
-end
