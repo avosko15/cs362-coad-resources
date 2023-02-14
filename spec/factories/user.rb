8de11d8621f292d10073f3c31f198dfc38a387c0
grassele <91402260+grassele@users.noreply.github.com>
2023-02-14 11:08:57 -0800

    fixed duplicate factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,18 @@
 FactoryBot.define do
     factory :user do
         email { 'bario54321@gmail.com' }
+        password { "fake_password" }
+
+        before(:create) { |user| user.skip_confirmation! }
+
+        trait :organization_approved do
+            role { :organization }
+            organization_id { create(:organization, :approved).id }
+        end
+
+        trait :organization_unapproved do
+            role { :organization }
+            organization_id { create(:organization).id }
+        end
     end
 end
