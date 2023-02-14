dd47d332877ba7d27b17dd367a04fa17cf6ff11a
grassele <91402260+grassele@users.noreply.github.com>
2023-02-14 10:28:49 -0800

    added factory file for tickets_controller based on beej's demo

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
+    # let(:ticket) { create(:ticket) }
+    # let(:organization_approved_user) { create(:user, :organization_approved_user) }
+    # let(:organization_unapproved_user) { create(:user, :organization_unapproved_user) }
 
 end
