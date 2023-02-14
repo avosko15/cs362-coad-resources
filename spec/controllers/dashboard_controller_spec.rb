e2af5f61d3e0abee9dd6852a97f9085b21f09cc6
grassele <91402260+grassele@users.noreply.github.com>
2023-02-14 10:18:37 -0800

    updated factory for dashboard_controller per beej's demo

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
+    describe "GET #index while logged in" do
+        let(:user) { create(:user) }
+        before(:each) { sign_in(user) }
+        it { expect(get(:index)).to be_successful }
+    end
 
+    describe "GET #index while logged out" do
+        it { expect(get(:idex)).to redirect_to(new_user_session_path) }
+    end
 end
