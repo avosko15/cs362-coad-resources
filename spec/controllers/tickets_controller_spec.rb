a8bd4593b0018408eb4abbcb01980a6d7fd12059
grassele <91402260+grassele@users.noreply.github.com>
2023-02-14 10:35:10 -0800

    more changes based on what beej showed

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
-    # let(:ticket) { create(:ticket) }
-    # let(:organization_approved_user) { create(:user, :organization_approved_user) }
-    # let(:organization_unapproved_user) { create(:user, :organization_unapproved_user) }
+    let(:ticket) { create(:ticket) }
+    let(:organization_approved_user) { create(:user, :organization_approved_user) }
+    let(:organization_unapproved_user) { create(:user, :organization_unapproved_user) }
+    
+    describe 'GET #new' do
+        it { expect(get(:new)).to be_successful }
+    end
 
+    describe 'GET #show' do
+        before do
+            sign_in organization_approved_user
+        end
+
+        it { expect(get(:show, params: { id: ticket.id })).to be_successful }
+    end
+
+    # not sure if there's more here
 end
