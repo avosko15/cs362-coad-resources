1ef49548f41ad1fa66b1380f820362f39f8447a6
Blake Pugh <bario54321@gmail.com>
2023-02-21 11:52:56 -0800

    GET #index working for resource_categories

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
+    context 'as a logged-in user' do
+        let(:user) { create(:user) }
+        before(:each) { sign_in(user) }
+
+        let(:resource_category) { create(:resource_category) }
+
+        describe "GET #index" do
+            it { expect(get(:index)).to redirect_to(dashboard_path)}
+        end
+    end
 
 end
