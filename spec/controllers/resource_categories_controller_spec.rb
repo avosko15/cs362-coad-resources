99f03ed7a277de09d239e71189b5006fbb871943
Blake Pugh <bario54321@gmail.com>
2023-02-22 13:22:17 -0800

    resource category POST #create tested, 175 passing, 0 failing

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -18,6 +18,13 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         describe "GET #new" do
             it { expect(get(:new)).to redirect_to(dashboard_path) }
         end
+
+        describe "POST #create" do
+            it {
+                post(:create, params: { resource_category: attributes_for(:resource_category) })
+                expect(response).to redirect_to(dashboard_path)
+            }
+        end
     end
 
     context 'as a logged out user' do
@@ -32,6 +39,14 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         describe "GET #new" do
             it { expect(get(:new)).to redirect_to(new_user_session_path) }
         end
+
+        describe "POST #create" do
+            it {
+                post(:create, params: { resource_category: attributes_for(:resource_category) })
+                expect(response).to redirect_to(new_user_session_path)
+            }
+        end
+
     end
 
     context 'as an admin' do
@@ -50,6 +65,12 @@ RSpec.describe ResourceCategoriesController, type: :controller do
             it { expect(get(:new)).to be_successful}
         end
         
+        describe "POST #create" do
+            it {
+                post(:create, params: { resource_category: attributes_for(:resource_category) })
+                expect(response).to redirect_to(resource_categories_path)
+            }
+        end
     end
 
 end
