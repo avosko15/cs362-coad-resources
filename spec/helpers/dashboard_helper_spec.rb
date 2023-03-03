d6d2bd0fc6e90610f3e020b7e7aa00a65fbfa964
grassele <91402260+grassele@users.noreply.github.com>
2023-03-03 11:09:50 -0800

    dashboard_helper_spec.rb

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -24,18 +24,32 @@ RSpec.describe DashboardHelper, type: :helper do
     end
 
     context "organization submitted" do
-        let(:user) { instance_double('User', admin?: false, organization: Organization.new(status: :submitted)) } # a third way of making this happen
+        # let(:user) { instance_double('User', admin?: false, organization: Organization.new(status: :submitted)) } # a third way of making this happen
 
         describe "gets the organization_submitted_dashboard" do
-            it { expect(helper.dashboard_for(user)).to eq 'organization_submitted_dashboard'}
+            let(:submitted_organization) { instance_double('Organization', status: :submitted) }
+            let(:user) { instance_double('User', admin?: false, organization: submitted_organization) }
+
+            it {
+                expect(submitted_organization).to receive(:submitted?).and_return(true)
+                allow(submitted_organization).to receive(:approved?).and_return(false)
+                expect(helper.dashboard_for(user)).to eq 'organization_submitted_dashboard'
+            }
         end
     end
 
     context "organization approved" do
-        let(:user) { instance_double('User', admin?: false, organization: Organization.new(status: :approved)) } # a third way of making this happen
+        # let(:user) { instance_double('User', admin?: false, organization: Organization.new(status: :approved)) }
 
         describe "gets the organization_approved_dashboard" do
-            it { expect(helper.dashboard_for(user)).to eq 'organization_approved_dashboard'}
+            let(:approved_organization) { instance_double('Organization', status: :approved) }
+            let(:user) { instance_double('User', admin?: false, organization: approved_organization) }
+
+            it { 
+                allow(approved_organization).to receive(:submitted?).and_return(false)
+                expect(approved_organization).to receive(:approved?).and_return(true)
+                expect(helper.dashboard_for(user)).to eq 'organization_approved_dashboard'
+            }
         end
     end
 
