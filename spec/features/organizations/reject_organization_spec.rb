827e65365b48f91c4bf753419e762b6cd78210e4
Blake Pugh <bario54321@gmail.com>
2023-03-08 13:23:43 -0800

    Reject organization capybara test happy path tested, 244 passing 0 failing

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
 
+    it 'can be done from the dashboard' do
+        user = create(:user, :admin)
+        organization = create(:organization) # get an organization ready to be approved
+        log_in_as(user)
+
+        visit dashboard_path # Naviagate to the dashboard
+
+        click_on 'Organizations'
+        click_on 'Pending' # works with or without this line (can find Review before selecting the menu option)
+        # if we start having issues with this, use save_and_open_page() to troubleshoot -- if you have multiple Review buttons, can 
+        click_on 'Review'
+        click_on 'Reject'
+
+        expect(organization.reload.rejected?).to be true
+    end
 end
