85244abd91f74cb3b2183127dea11171800d5e7b
Blake Pugh <bario54321@gmail.com>
2023-03-09 10:03:38 -0800

    Reject organization 1st test, create organization test, but not working

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
-    
+    it 'can be tested' do
+        user = create(:user)
+        log_in_as(user)
+
+        visit dashboard_path
+        click_on 'Create Application'
+
+        choose 'Yes'
+        choose 'I Agree'
+        choose 'I Agree'
+        choose 'I Agree'
+        choose 'I Agree'
+        choose 'I Agree'
+        choose 'I Agree'
+        choose 'I Agree'
+        choose 'I Agree'
+        
+        fill_in 'What is your name?', with: 'Pugh, Blake'
+        fill_in 'Organization Name', with: 'Temp Organization'
+        fill_in 'What is your direct phone number', with: '503-851-0390'
+        fill_in "Who may we contact regarding your organization's application", with: "Meeee"
+        fill_in 'What is a good secondary phone number', with: '555-555-5555'
+        fill_in "What is your Organization's email?", with: 'test@example.com'
+        
+        select 'Test', from: 'Select-box'
+        
+        fill_in 'Description', with: 'this is a test'
+        
+        choose 'Yes'
+
+        expect(user.organization.submitted?).to be true
+    end
 end
