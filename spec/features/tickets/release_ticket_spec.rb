0415091e58a55887f8cda56abcee7cc27bb3722f
grassele <91402260+grassele@users.noreply.github.com>
2023-03-09 12:04:12 -0800

    release_ticket feature tested

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe 'Releasing a ticket by an', type: :feature do
+RSpec.describe 'Releasing a ticket', type: :feature do
+
+    it 'can be done by an approved organization' do
+        user = create(:user, :organization_approved_user)
+        ticket = create(:ticket, :req_fields_ticket)
+
+        log_in_as(user)
+
+        visit dashboard_path
+
+        click_on 'Tickets'
+        click_on 'Sample ticket user name'
+        click_on 'Capture'
+
+        visit dashboard_path
+
+        click_on 'Tickets'
+        click_on 'Sample ticket user name'
+        click_on 'Release'
+
+        visit dashboard_path
+
+        click_on 'Tickets'
+        click_on 'Sample ticket user name'
+
+        expect('Captured by').to_not have_content(user.organization.name)
+    end
 
 end
