11b51f746f7ebc7df1660b6ab2109ef147cd9516
Anna <73409556+avosko15@users.noreply.github.com>
2023-03-10 12:05:14 -0800

    user reg and user login spec done

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
     it 'successfully logsin a user' do
-        
         user = create(:user)
         visit dashboard_path
-        click_on 'Log in'
+        find("a[href='#{"/login"}']").click
+        fill_in 'Email address', with: 'test@fakeassemail.com'
+        fill_in 'Password', with: 'beepbeep'
+        click_on 'commit'
     end
 end
