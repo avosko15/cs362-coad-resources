aaa8ef97e6d8eb0a2e92474bf5b64835cc97d2b3
Anna <73409556+avosko15@users.noreply.github.com>
2023-03-10 11:00:03 -0800

    delete resourcecat spec done

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
+    let(:admin) { create(:user, :admin) }
 
+    it 'successfully deletes a resource category' do
+        #user = create(:user, :admin)
+        resource_category = create(:resource_category)
+        log_in_as(admin)
+
+        visit resource_categories_path
+
+        click_on resource_category.name
+        click_on 'Delete'
+        expect(current_path).to eq resource_categories_path
+        expect(page).to have_text('Unspecified')
+    end
 end
